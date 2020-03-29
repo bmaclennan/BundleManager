e883677390b4c496fc3ee7632711a8d910dcca3a
Brittney <bje3dm@mail.umck.edu>
2020-03-29 16:46:02 -0500

    Updated for iteration 3.

--- app/src/main/java/com/example/bundlemanager/openCraftsBundle.java
+++ app/src/main/java/com/example/bundlemanager/openCraftsBundle.java
@@ -2,7 +2,10 @@ package com.example.bundlemanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class openCraftsBundle extends AppCompatActivity {
 
@@ -10,5 +13,19 @@ public class openCraftsBundle extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open_crafts_bundle);
+
+        Button springButton = findViewById(R.id.spForaging);
+
+        springButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSpringForaging();
+            }
+        });
+    }
+
+    public void openSpringForaging() {
+        Intent intent = new Intent(this, openSpringForaging.class);
+        startActivity(intent);
     }
 }
